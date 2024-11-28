ebb66b0a358bc877f55616f3261f5d4a44297c6a
david Murage <davidmurage415@gmail.com>
2024-11-28 09:19:06 +0300

    updates

--- ArtisanMarketPlace/settings.py
+++ ArtisanMarketPlace/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Location where Django collects all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
