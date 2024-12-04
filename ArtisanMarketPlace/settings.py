9a0b822d0aa87972876a2c311706f58b802693b3
david Murage <davidmurage415@gmail.com>
2024-12-04 15:32:43 +0300

    updated settings.py and .env

--- ArtisanMarketPlace/settings.py
+++ ArtisanMarketPlace/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,9 @@ MEDIA_ROOT = BASE_DIR/ 'media' # Directory where uploaded files are stored
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t78dz$=zix#z8yve#wb!xu@ikep@#)o&5bp1hrtqz=g+=5($$7'
+
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
