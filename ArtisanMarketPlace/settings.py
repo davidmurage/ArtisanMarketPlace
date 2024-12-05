7954fbeea1f7e61803e9c05580760cfdabfe43e7
david Murage <davidmurage415@gmail.com>
2024-12-05 09:20:52 +0300

    updated settings

--- ArtisanMarketPlace/settings.py
+++ ArtisanMarketPlace/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 
 # Application definition
