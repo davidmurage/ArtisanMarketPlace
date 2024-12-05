03521a68328619ccb913784ecca1ef724a1ddf63
david Murage <davidmurage415@gmail.com>
2024-12-05 09:44:40 +0300

    updated settings.py

--- ArtisanMarketPlace/settings.py
+++ ArtisanMarketPlace/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['artisanmarketplace.onrender.com/', 'localhost']
+ALLOWED_HOSTS = ['artisanmarketplace.onrender.com', 'localhost']
 
 
 # Application definition
